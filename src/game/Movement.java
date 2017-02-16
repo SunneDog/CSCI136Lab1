098c443c1d8e85fa39bb192c642e95fe918588af
Danni Sunne <etnichols55@gmail.com>
2017-02-16 14:19:46 -0700

    1.1 Prep for 2

--- src/game/Movement.java
+++ src/game/Movement.java
@@ -2,20 +2,32 @@
 // Purpose: this class controls the movement of players.
 package game;
 
-public class Movement {
+import java.awt.event.KeyEvent;
+
+public class Movement  {
 	private int newXPos;
 	private int newYPos;
 	private final int DIST = 100; // distance player moves per call of method.
-	
 	// constructor
-	
+	public Movement() {
+		 
+	}
 	// getters + setters
 	public int getNewXPos(int xPos) {
-		newXPos = xPos + DIST;
+		if(GamePanel.getKeyCode() == KeyEvent.VK_RIGHT || GamePanel.getKeyCode() == KeyEvent.VK_D) {
+			newXPos = xPos += DIST;
+		} else if(GamePanel.getKeyCode() == KeyEvent.VK_LEFT || GamePanel.getKeyCode() == KeyEvent.VK_A) {
+			newXPos = xPos -= DIST;
+		}
 		return newXPos;
 	}
 	public int getNewYPos(int yPos) {
-		newYPos = yPos + DIST;
+		if(GamePanel.getKeyCode() == KeyEvent.VK_UP || GamePanel.getKeyCode() == KeyEvent.VK_W) {
+			newYPos = yPos += DIST;
+		} else if(GamePanel.getKeyCode() == KeyEvent.VK_DOWN || GamePanel.getKeyCode() == KeyEvent.VK_S) {
+			newYPos = yPos -= DIST;
+		}
+		
 		return newYPos;
 	}
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -2,9 +2,13 @@
 // Purpose: Contains all the information about the player, their
 // 		poisition, and their score. It also has an instance of
 // 		Movement to manage motion on a player basis.
+//		Can read in its attributes from a file
 package game;
 
 import javax.swing.ImageIcon;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Player {
 	private String name;
@@ -14,14 +18,31 @@ public class Player {
 	private int yPos;
 	private Movement mover;
 	
-	// constructor
+	// constructors
 	public Player(String name, ImageIcon icon, int xPos, int yPos) {
 		this.name = name;
 		this.icon = icon;
 		this.xPos = xPos;
 		this.yPos = yPos;
 	}
+	public Player() {
+		
+	}
+	// 
 	
+	public void readFromFile(File file) throws FileNotFoundException {
+		Scanner fileScanner = new Scanner(file);
+		fileScanner.useDelimiter(",");
+		String name;
+		ImageIcon icon;
+		while(fileScanner.hasNext()) {
+			name = fileScanner.next();
+			icon = new ImageIcon(fileScanner.next());
+			this.setName(name);
+			this.setIcon(icon);
+		}
+		fileScanner.close();
+	}
 	// getters + setters
 	public void setName(String name) {
 		this.name = name;
