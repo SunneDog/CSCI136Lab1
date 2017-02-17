6a99ed0960f4c7d32777dbba661a37b82a471dfe
Danni Sunne <etnichols55@gmail.com>
2017-02-16 20:44:38 -0700

    Lab2_final

--- src/game/Movement.java
+++ src/game/Movement.java
@@ -7,27 +7,53 @@ import java.awt.event.KeyEvent;
 public class Movement  {
 	private int newXPos;
 	private int newYPos;
-	private final int DIST = 100; // distance player moves per call of method.
+	private final int DIST = 50; // distance player moves per call of method.
 	// constructor
 	public Movement() {
 		 
 	}
-	// getters + setters
+	
+	
 	public int getNewXPos(int xPos) {
-		if(GamePanel.getKeyCode() == KeyEvent.VK_RIGHT || GamePanel.getKeyCode() == KeyEvent.VK_D) {
-			newXPos = xPos += DIST;
-		} else if(GamePanel.getKeyCode() == KeyEvent.VK_LEFT || GamePanel.getKeyCode() == KeyEvent.VK_A) {
-			newXPos = xPos -= DIST;
-		}
+		calcNewXPos(xPos);
 		return newXPos;
 	}
-	public int getNewYPos(int yPos) {
-		if(GamePanel.getKeyCode() == KeyEvent.VK_UP || GamePanel.getKeyCode() == KeyEvent.VK_W) {
-			newYPos = yPos += DIST;
-		} else if(GamePanel.getKeyCode() == KeyEvent.VK_DOWN || GamePanel.getKeyCode() == KeyEvent.VK_S) {
-			newYPos = yPos -= DIST;
+	private void calcNewXPos(int xPos) {
+		if(GamePanel.getKeyCode() == KeyEvent.VK_RIGHT || GamePanel.getKeyCode() == KeyEvent.VK_D) {
+			if(xPos == 650) {
+				// stops them from leaving the screen	
+				} else {
+					newXPos = xPos += DIST;
+					System.out.println("Player moved right!");
+				}
+		} else if(GamePanel.getKeyCode() == KeyEvent.VK_LEFT || GamePanel.getKeyCode() == KeyEvent.VK_A) {
+			if(xPos == 0) {
+				// stops them from leaving the screen	
+				} else {
+					newXPos = xPos -= DIST;
+					System.out.println("Player moved left!");
+				}
 		}
-		
+	}
+	public int getNewYPos(int yPos) {
+		calcNewYPos(yPos);
 		return newYPos;
+	}
+	private void calcNewYPos(int yPos) {
+		if(GamePanel.getKeyCode() == KeyEvent.VK_UP || GamePanel.getKeyCode() == KeyEvent.VK_W) {
+			if(yPos == 0) {
+			// stops them from leaving the screen	
+			} else {
+				newYPos = yPos -= DIST;
+				System.out.println("Player moved up!");
+			}
+		} else if(GamePanel.getKeyCode() == KeyEvent.VK_DOWN || GamePanel.getKeyCode() == KeyEvent.VK_S) {
+			if(yPos == 650) {
+				// stops them from leaving the screen	
+				} else {
+					newYPos = yPos += DIST;
+					System.out.println("Player moved down");
+				}
+		}
 	}
 }
